b0515e702f5dd4a0ac88b73d7ec1c0e9a8395810
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2025-05-26 03:10:39 +0200

    Lab3 task 1 done

---
+++ Lab3/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+ProdContext prodContext = new ProdContext();
+Console.WriteLine("Podaj nazwę produktu: ");
+String? prodName = Console.ReadLine();
+Product product = new Product { ProductName = prodName };
+prodContext.Products.Add(product);
+prodContext.SaveChanges();
+
+var query = from prod in prodContext.Products
+            select prod.ProductName;
+
+foreach (var pName in query)
+{
+    Console.WriteLine(pName);
+}
